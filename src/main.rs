b7d7bc38694fb03522182b2108c96a4f42d541e2
ritiek <ritiekmalhotra123@gmail.com>
2017-10-02 15:20:07 +0530

    Add black key graphics

--- src/main.rs
+++ src/main.rs
@@ -18,21 +18,30 @@ fn main() {
     /* white keys */
     for y in 0..16 {
         for x in 0..52 {
-            let key = x*3;
-            rustbox.print(key, y, rustbox::RB_BOLD, Color::White, Color::Black, " ██");
-            rustbox.print(key, y, rustbox::RB_BOLD, Color::Black, Color::White, "|");
+            let k = x*3;
+            rustbox.print(k, y, rustbox::RB_BOLD, Color::White, Color::Black, " ██");
+            rustbox.print(k, y, rustbox::RB_BOLD, Color::Black, Color::White, "|");
         }
     }
 
     /*black keys */
     for y in 0..9 {
+        //1st black key is lonely
         rustbox.print(3, y, rustbox::RB_BOLD, Color::Black, Color::White, "█");
+
         for x in 0..7 {
-            let g1k1 = (x*21) + 9;
+            let g1k1 = x*21 + 9;
             let g1k2 = g1k1 + 3;
             rustbox.print(g1k1, y, rustbox::RB_BOLD, Color::Black, Color::White, "█");
             rustbox.print(g1k2, y, rustbox::RB_BOLD, Color::Black, Color::White, "█");
+            let g2k1 = g1k2 + 6;
+            let g2k2 = g2k1 + 3;
+            let g2k3 = g2k2 + 3;
+            rustbox.print(g2k1, y, rustbox::RB_BOLD, Color::Black, Color::White, "█");
+            rustbox.print(g2k2, y, rustbox::RB_BOLD, Color::Black, Color::White, "█");
+            rustbox.print(g2k3, y, rustbox::RB_BOLD, Color::Black, Color::White, "█");
         }
+
     }
 
     loop {
