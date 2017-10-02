c9ea5bc3174aecc65c169eb9d8c9d9850465514d
ritiek <ritiekmalhotra123@gmail.com>
2017-10-02 14:35:30 +0530

    White keys graphic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,39 @@
 extern crate rustbox;
 
+use std::default::Default;
+
 use rustbox::{Color, RustBox};
 use rustbox::Key;
+
+/*
+█▒
+*/
+
+fn main() {
+    let rustbox = match RustBox::init(Default::default()) {
+        Result::Ok(v) => v,
+        Result::Err(e) => panic!("{}", e),
+    };
+
+    for y in 0..16 {
+        for x in 0..52 {
+            rustbox.print(x*3, y, rustbox::RB_BOLD, Color::White, Color::Black, " ██");
+            rustbox.print(x*3, y, rustbox::RB_BOLD, Color::Black, Color::White, "|");
+        }
+    }
+
+    loop {
+        rustbox.present();
+        match rustbox.poll_event(false) {
+            Ok(rustbox::Event::KeyEvent(key)) => {
+                match key {
+                    Key::Char('q') => { break; }
+                    _ => { }
+                }
+            },
+            Err(e) => panic!("{}", e),
+            _ => { }
+        }
+    }
+}
+
