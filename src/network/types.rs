96ef903d2958f6d642a8a222f02eb27f36dc01d9
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2019-09-23 20:23:45 +0530

    Receiver and Sender network implementations in their own files

---
+++ src/network/types.rs
@@ -0,0 +1,20 @@
+use serde_derive::{Serialize, Deserialize};
+use std::net::SocketAddr;
+pub use crate::game::{Note, GameEvent};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum NetworkEvent {
+    ID(usize),
+    Note(Note),
+    Peers(Vec<SocketAddr>),
+    PlayerJoin,
+    PlayerLeft,
+}
+
+#[derive(Debug)]
+pub struct NetworkData {
+    pub amt: usize,
+    pub src: SocketAddr,
+    pub event: NetworkEvent,
+}
